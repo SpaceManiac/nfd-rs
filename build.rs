d9ae53d9f375f38fc2fff71b8ccf0dc7fb039322
Tad Hardesty <tad@platymuus.com>
2019-11-14 18:18:02 -0800

    Switch build.rs to use nfd_zenity.c

--- build.rs
+++ build.rs
@@ -22,7 +22,6 @@ THE SOFTWARE.
 
 extern crate cc;
 use std::env;
-use std::process::Command;
 
 macro_rules! nfd {
     ($suf:expr) => {
@@ -50,30 +49,7 @@ fn main() {
         // MinGW doesn't link it by default
         println!("cargo:rustc-link-lib=uuid");
     } else {
-        let pkg_output = Command::new("pkg-config")
-            .arg("--cflags")
-            .arg("gtk+-3.0")
-            .arg("glib-2.0")
-            .arg("--libs")
-            .arg("glib-2.0")
-            .output();
-        match pkg_output {
-            Ok(output) => {
-                let t = String::from_utf8(output.stdout).unwrap();
-                let flags = t.split(" ");
-                for flag in flags {
-                    if flag != "\n" && flag != "" {
-                        cfg.flag(flag);
-                    }
-                }
-            }
-            _ => (),
-        }
-        cfg.file(nfd!("nfd_gtk.c"));
+        cfg.file(nfd!("nfd_zenity.c"));
         cfg.compile("libnfd.a");
-        println!("cargo:rustc-link-lib=gdk-3");
-        println!("cargo:rustc-link-lib=gtk-3");
-        println!("cargo:rustc-link-lib=glib-2.0");
-        println!("cargo:rustc-link-lib=gobject-2.0");
     }
 }
